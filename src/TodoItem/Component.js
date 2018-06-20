0b565bcd91d3630754dff775ccca9ab23a36af1f
David Yeung <quxbaz@gmail.com>
2018-06-19 22:21:20 -0600

    styling mostly

--- src/TodoItem/Component.js
+++ src/TodoItem/Component.js
@@ -1,28 +1,31 @@
 import style from './style.css'
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 class TodoItem extends Component {
 
-  handleToggle = () => {
+  handleClick = () => {
     this.props.onToggle(this.props.todo)
   }
 
-  handleRemove = () => {
+  handleRemove = (event) => {
+    event.stopPropagation()
     this.props.onClickRemove(this.props.todo)
   }
 
   render () {
     const {todo} = this.props
+    const className = classNames(style.TodoItem, {
+      [style.isCompleted]: todo.isCompleted,
+    })
     return (
-      <div className={style.TodoItem}>
+      <div className={className} onClick={this.handleClick}>
+        <div className={style.CheckMark}>✔</div>
         <div style={{marginRight: '6px'}}>
           {this.props.i + 1}.
         </div>
-        <div className={style.ColLeft}>
-          <input className={style.Checkbox} type='checkbox' checked={todo.isCompleted} onChange={this.handleToggle} />
-          {todo.text}
-        </div>
+        <div className={style.ColLeft}>{todo.text}</div>
         <div className={style.ColRight}>
           <button className={style.Remove} onClick={this.handleRemove}>remove</button>
         </div>
